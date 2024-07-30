3f437365d4e85ad95f11a1b086a379d97d9d4adc
3405000 <myuserlee@gmail.com>
2024-07-30 14:42:10 +0900

    [Design] #4 - 테스트 하면서 파일 생성해보았어요

---
+++ BOOKBBANG/BOOKBBANG/Views/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BOOKBBANG
+//
+//  Created by Jerrie on 7/30/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("제리가 만든 홈 뷰입니다")
+    }
+}
+
+#Preview {
+    HomeView()
+}
