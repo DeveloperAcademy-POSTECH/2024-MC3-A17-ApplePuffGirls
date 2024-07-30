f7ce35004ede8d865e07829b2e9c3615e3f5ab50
kukikong <kukikongmini@naver.com>
2024-07-30 14:42:04 +0900

    [Test] #6 - 깃허브 테스트하며 폴더 생성

---
+++ BOOKBBANG/BOOKBBANG/Views/Onboarding/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  BOOKBBANG
+//
+//  Created by 강다혜 on 7/30/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
