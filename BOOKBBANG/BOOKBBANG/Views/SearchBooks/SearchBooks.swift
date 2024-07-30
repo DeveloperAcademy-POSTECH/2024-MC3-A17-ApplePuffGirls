ba68f99202cd2b7cec21407e337e260897d26d6f
Guryss <lala1470@konkuk.ac.kr>
2024-07-30 14:02:33 +0900

    [Setting] project foldering

---
+++ BOOKBBANG/BOOKBBANG/Views/SearchBooks/SearchBooks.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBooks.swift
+//  BOOKBBANG
+//
+//  Created by Seoyeon Choi on 7/30/24.
+//
+
+import SwiftUI
+
+struct SearchBooks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBooks()
+}
