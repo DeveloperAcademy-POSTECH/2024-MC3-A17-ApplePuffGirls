1679bd1ea8238e1a3a049be53fe88cf8d3a8a24b
Guryss <lala1470@konkuk.ac.kr>
2024-07-30 12:38:33 +0900

    [Setting] project setting & add gitignore

---
+++ BOOKBBANG/BOOKBBANG/BOOKBBANGApp.swift
@@ -0,0 +1,20 @@
+//
+//  BOOKBBANGApp.swift
+//  BOOKBBANG
+//
+//  Created by Seoyeon Choi on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BOOKBBANGApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
