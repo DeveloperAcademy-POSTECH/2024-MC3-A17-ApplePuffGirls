fdb08b87363cbaf207fd00459199b0c28e086d9b
Guryss <lala1470@konkuk.ac.kr>
2024-08-01 03:14:43 +0900

    [Design] #19 - 직접 추가하기 버튼 UI 구현

--- BOOKBBANG/BOOKBBANG/Views/Book/SearchBook/SearchBook.swift
+++ BOOKBBANG/BOOKBBANG/Views/Book/SearchBook/SearchBook.swift
@@ -12,37 +12,55 @@ struct SearchBook: View {
     @State private var isSelected: Bool = false
     
     var body: some View {
-        VStack(spacing: 0) {
-            SearchBookHeader()
-            
-            SearchBookProgressBar()
-                .padding(.bottom, 77)
-            
-            
-            SearchBookSearchBar()
-                .padding(.horizontal, 22)
-                .padding(.bottom, 30)
-            
-            ScrollView {
-                Spacer().frame(height: 20)
+        NavigationStack {
+            VStack(spacing: 0) {
+                SearchBookHeader()
                 
-                ForEach(0..<5) { _ in
-                    SearchBookListRow(isSelected: $isSelected)
-                        .padding(.bottom, 25)
-                        .padding(.leading, 20)
+                SearchBookProgressBar()
+                    .padding(.bottom, 77)
+                
+                
+                SearchBookSearchBar()
+                    .padding(.horizontal, 22)
+                    .padding(.bottom, 30)
+                
+                ScrollView {
+                    Spacer().frame(height: 20)
                     
-                    VStack{}
-                        .frame(maxWidth: UIScreen.main.bounds.width)
-                        .frame(height: 0.5)
-                        .background(.divider)
-                        .padding(.bottom, 25)
-                        .padding(.horizontal, 14)
+                    ForEach(0..<5) { _ in
+                        SearchBookListRow(isSelected: $isSelected)
+                            .padding(.bottom, 25)
+                            .padding(.leading, 20)
+                        
+                        VStack{}
+                            .frame(maxWidth: UIScreen.main.bounds.width)
+                            .frame(height: 0.5)
+                            .background(.divider)
+                            .padding(.bottom, 25)
+                            .padding(.horizontal, 14)
+                    }
+                    
+                    HStack {
+                        Text("혹시 원하는 책이 없나요?")
+                            .foregroundStyle(.typo50)
+                            .font(.system(size: 13, weight: .regular))
+                        
+                        Spacer()
+                        
+                        NavigationLink(destination: {
+                            fetchImage(url: "https://i.pinimg.com/564x/88/cd/0b/88cd0beb38989c7f9418f0f06449fc32.jpg")
+                        }, label: {
+                            DirectRegisterBookButton()
+                        })
+                    }
+                    .padding(.horizontal, 25)
+                    .padding(.bottom, 40)
                 }
+                
+                .scrollIndicators(.hidden)
             }
-            
-            .scrollIndicators(.hidden)
+            .background(.backLighter)
         }
-        .background(.backLighter)
     }
 }
 
@@ -54,6 +72,18 @@ func SearchBookProgressBar() -> some View {
             VStack{}
                 .frame(width: UIScreen.main.bounds.width / 3, height: 2)
                 .background(.greenMain100)
+        }
+}
+
+func DirectRegisterBookButton() -> some View {
+    return RoundedRectangle(cornerRadius: 29)
+        .stroke(.greenMain100, lineWidth: 1.0)
+        .frame(width: 120 ,height: 32)
+        .overlay {
+            Text("+  직접 추가하기")
+                .foregroundStyle(.greenMain100)
+                .font(.system(size: 13, weight: .regular))
+                .padding(.horizontal, 10)
         }
 }
 
