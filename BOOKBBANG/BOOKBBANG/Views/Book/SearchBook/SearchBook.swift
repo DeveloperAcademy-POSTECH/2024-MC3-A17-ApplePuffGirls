cb25c50d16f26575c5e9f57b1970060852ad9493
Guryss <lala1470@konkuk.ac.kr>
2024-08-01 00:04:49 +0900

    [Design] #19 - SearchBookSearchBar UI 구현

--- BOOKBBANG/BOOKBBANG/Views/Book/SearchBook/SearchBook.swift
+++ BOOKBBANG/BOOKBBANG/Views/Book/SearchBook/SearchBook.swift
@@ -10,9 +10,28 @@ import UIKit
 
 struct SearchBook: View {
     var body: some View {
-        Text("Search Books")
-            .font(.bookk15)
+        VStack(spacing: 0) {
+            SearchBookHeader()
+            SearchBookProgressBar()
+                .padding(.bottom, 77)
+            
+            SearchBookSearchBar()
+                .padding(.horizontal, 22)
+            
+            Spacer()
+        }
     }
+}
+
+func SearchBookProgressBar() -> some View {
+    return VStack {}
+        .frame(width: UIScreen.main.bounds.width, height: 2)
+        .background(.typo25)
+        .overlay(alignment: .leading) {
+            VStack{}
+                .frame(width: UIScreen.main.bounds.width / 3, height: 2)
+                .background(.greenMain100)
+        }
 }
 
 #Preview {
