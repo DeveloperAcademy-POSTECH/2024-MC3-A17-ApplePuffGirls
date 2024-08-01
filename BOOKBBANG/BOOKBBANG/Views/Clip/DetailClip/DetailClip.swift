c98590f54de7d0fa2d6473cd5401d9ed9127afb5
kukikong <kukikongmini@naver.com>
2024-08-01 15:03:32 +0900

    [Design] #24 - DetailClip 파일추가

---
+++ BOOKBBANG/BOOKBBANG/Views/Clip/DetailClip/DetailClip.swift
@@ -0,0 +1,18 @@
+//
+//  DetailClip.swift
+//  BOOKBBANG
+//
+//  Created by 강다혜 on 8/1/24.
+//
+
+import SwiftUI
+
+struct DetailClip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailClip()
+}
