89788557a625d7cb560c57a3211b1401a93784e3
Guryss <lala1470@konkuk.ac.kr>
2024-08-03 22:33:21 +0900

    [Chore] #37 - 에셋 파일들 폴더링

---
+++ BOOKBBANG/BOOKBBANG/Views/Book/SearchBook/SearchTestView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTestView.swift
+//  BOOKBBANG
+//
+//  Created by Seoyeon Choi on 8/3/24.
+//
+
+import SwiftUI
+
+struct SearchTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchTestView()
+}
---
+++ BOOKBBANG/BOOKBBANG/Views/Helpers/PersistentController.swift
@@ -0,0 +1,8 @@
+//
+//  PersistantController.swift
+//  BOOKBBANG
+//
+//  Created by Seoyeon Choi on 8/3/24.
+//
+
+import Foundation
