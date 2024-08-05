60829cb1e6a075f193f64ba830ec69b5e4ba7cbf
Gimli Kim <infinite1st@gmail.com>
2024-08-05 20:56:52 +0900

    [Design] #34 카테고리 수정뷰 그렸습니다. 겹치는 뷰가 있어서 따로 나눴습니다.

---
+++ BOOKBBANG/BOOKBBANG/Views/Phrase/AddCategoryToPhrase/MultipleClipSelectList.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleClipSelectList.swift
+//  BOOKBBANG
+//
+//  Created by kyunglimkim on 8/5/24.
+//
+
+import SwiftUI
+
+struct MultipleClipSelectList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleClipSelectList()
+}
---
+++ BOOKBBANG/BOOKBBANG/Views/Phrase/EditPhrase/EditClipinPhrase.swift
@@ -0,0 +1,18 @@
+//
+//  EditClipinPhrase.swift
+//  BOOKBBANG
+//
+//  Created by kyunglimkim on 8/5/24.
+//
+
+import SwiftUI
+
+struct EditClipinPhrase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditClipinPhrase()
+}
