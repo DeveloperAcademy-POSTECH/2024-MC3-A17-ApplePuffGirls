c9b4e31efe8536179262496011fbc258512ffeec
Rochelle0922 <yeonjun6193@naver.com>
2024-08-05 16:10:21 +0900

    [Refactor]#5 구리스가 enum 합치라고 해서 코드 수정했습니당

--- BOOKBBANG/BOOKBBANG/Views/Clip/AddClip/ClipShapeButton.swift
+++ BOOKBBANG/BOOKBBANG/Views/Clip/AddClip/ClipShapeButton.swift
@@ -11,31 +11,33 @@ struct ClipShapeButton: View {
   @ObservedObject var clipData = ClipData()
   
   var body: some View {
-    Text("클립 디자인")
-      .font(.bookCaption)
-      .foregroundStyle(.typo50)
-      .padding(.trailing, 300)
-      .padding(.top, 15)
-    
-    VStack(spacing: 12) {
-      ForEach(0..<2) { row in
-        HStack(spacing: 12) {
-          ForEach(0..<4) { column in
-            let index = row * 4 + column
-            if index < ClipShapeImage.allCases.count {
-              let shape = ClipShapeImage.allCases[index]
-              Button(action: {
-                clipData.selectedShape = index
-              }) {
-                RoundedRectangle(cornerRadius: 10)
-                  .strokeBorder(clipData.selectedShape == index ? Color.green : Color.gray.opacity(0.5), lineWidth: clipData.selectedShape == index ? 2 : 1)
-                  .background(
-                    Image(shape.imageName)
-                      .resizable()
-                      .scaledToFit()
-                      .padding(12)
-                  )
-                  .frame(width: 80, height: 80)
+    VStack(alignment: .leading) {
+      Text("클립 디자인")
+        .font(.bookCaption)
+        .foregroundStyle(.typo50)
+        .padding(.trailing, 300)
+        .padding(.top, 15)
+      
+      VStack(spacing: 12) {
+        ForEach(0..<2) { row in
+          HStack(spacing: 12) {
+            ForEach(0..<4) { column in
+              let index = row * 4 + column
+              if index < ClipItem.allCases.count {
+                let shape = ClipItem.allCases[index]
+                Button(action: {
+                  clipData.selectedShape = index
+                }) {
+                  RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(clipData.selectedShape == index ? Color.green : Color.gray.opacity(0.5), lineWidth: clipData.selectedShape == index ? 2 : 1)
+                    .background(
+                      Image(shape.shapeImageName)  // 수정된 부분
+                        .resizable()
+                        .scaledToFit()
+                        .padding(12)
+                    )
+                    .frame(width: 80, height: 80)
+                }
               }
             }
           }
--- BOOKBBANG/BOOKBBANG/Views/Clip/AddClip/TopClipImage.swift
+++ BOOKBBANG/BOOKBBANG/Views/Clip/AddClip/TopClipImage.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct TopClipImage: View {
-  @ObservedObject var clipData = ClipData()
-  
-  var body: some View {
-    Image(clipData.selectedShape != nil ? ClipImage.allCases[clipData.selectedShape!].imageName : ClipImage.WaterDropClip.imageName)
-      .renderingMode(.template)
-      .resizable()
-      .frame(width: 115, height: 110)
-      .padding(.vertical, 50)
-      .foregroundColor(clipData.selectedColor != nil ? Colors.allCases[clipData.selectedColor!].color : Colors.cred.color)
-  }
+    @ObservedObject var clipData = ClipData()
+    
+    var body: some View {
+        Image(clipData.selectedShape != nil ? ClipItem.allCases[clipData.selectedShape!].clipImageName : ClipItem.WaterDrop.clipImageName)
+            .renderingMode(.template)
+            .resizable()
+            .frame(width: 115, height: 110)
+            .padding(.vertical, 50)
+            .foregroundColor(clipData.selectedColor != nil ? Colors.allCases[clipData.selectedColor!].color : Colors.cred.color)
+    }
 }
 
 #Preview {
-  TopClipImage()
+    TopClipImage()
 }
