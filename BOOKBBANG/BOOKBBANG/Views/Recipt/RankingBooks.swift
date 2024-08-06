4cc799c9a758f21971b3d2b14498d36fccff0268
3405000 <myuserlee@gmail.com>
2024-08-06 12:11:01 +0900

    [Design] #35 - 빵수증 뷰 수정수정

--- BOOKBBANG/BOOKBBANG/Views/Recipt/RankingBooks.swift
+++ BOOKBBANG/BOOKBBANG/Views/Recipt/RankingBooks.swift
@@ -8,25 +8,36 @@
 import SwiftUI
 
 struct RankingBooks: View {
+    @State var rankedBooks: [String] = []
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("인상깊은 책들을 선택해주세요")
+        VStack(spacing: 0) {
+            CustomNavigationBar(isHighlighted: .constant(false),
+                                navigationType: .chevron,
+                                title: "책 순위 선정하기", rightTitle: "다음")
             
-            Text("카테고리에 맞는 클립을 선택해보세요. (복수선택 가능) 다시 클립을 바꿀 수 있어요.")
+            HeaderSection(title: "인상깊은 책들을 선택해주세요", subtitle: "카테고리에 맞는 클립을 선택해보세요. (복수선택 가능) 다시 클립을 바꿀 수 있어요.")
             
             HStack(spacing: 14) {
                 ForEach(1..<4) { i in
                     EmptyBox(width: 106, height: 155, text: "\(i)위", isButton: false)
                 }
             }
+            .padding(.top, 30)
             
             ScrollView {
-                Text("내가 읽은 책빵 목록")
-                ImagesContainer()
+                VStack(alignment: .leading) {
+                    Text("내가 읽은 책빵 목록")
+                        .font(.bookCaption)
+                        .foregroundStyle(.typo50)
+                        .padding(.top, 15)
+                        .padding(.leading, 30)
+                    
+                    ImagesContainer()
+                }
             }
         }
-        .navigationTitle("책 순위 선정하기")
-        .navigationBarTitleDisplayMode(.inline)
+        .background(.backLighter)
     }
 }
 
