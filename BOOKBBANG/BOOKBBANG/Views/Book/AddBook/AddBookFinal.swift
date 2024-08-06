f81d3de05b170cc815bfa1046710308626415c25
kukikong <kukikongmini@naver.com>
2024-08-06 22:30:34 +0900

    [Setting] #54 - Assets 이미지 넣기

--- BOOKBBANG/BOOKBBANG/Views/Book/AddBook/AddBookFinal.swift
+++ BOOKBBANG/BOOKBBANG/Views/Book/AddBook/AddBookFinal.swift
@@ -8,21 +8,19 @@
 import SwiftUI
 
 struct AddBookFinal: View {
-  var body: some View {
-    
-    VStack{
-      HeaderSection(title: "책 추가를 완료했습니다",
-                    subtitle: "맛있는 빵을 만들러 가볼까요?")
-      .padding(.top, 150)
-      .padding(.bottom, 50)
-      
-      Image(.shakeDough)
-     
+    var body: some View {
+        VStack{
+            HeaderSection(title: "책 추가를 완료했습니다",
+                          subtitle: "맛있는 빵을 만들러 가볼까요?")
+            .padding(.top, 150)
+            .padding(.bottom, 50)
+            
+            Image(.baking2)
+        }
+        Spacer()
     }
-    Spacer()
-  }
 }
 
 #Preview {
-  AddBookFinal()
+    AddBookFinal()
 }
