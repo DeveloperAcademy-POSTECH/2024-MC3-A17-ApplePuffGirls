730ba03196d7b7b240710388745d79ba652a82f8
Guryss <lala1470@konkuk.ac.kr>
2024-08-07 14:53:53 +0900

    [Chore] #56 - BookInfoSection 레이아웃 잔잔바리 수정

--- BOOKBBANG/BOOKBBANG/Views/Book/AddBook/ BookInfoSection.swift
+++ BOOKBBANG/BOOKBBANG/Views/Book/AddBook/ BookInfoSection.swift
@@ -15,63 +15,66 @@ struct BookInfoSection: View {
     var book: Book
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.backLighter)
-                .frame(width: 380, height: 180)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.typo25, lineWidth: 1)
-                )
-                .padding(.top, 20)
-            
-            HStack {
-                fetchHomeImage(url: book.thumbnail ?? "")
-                    .padding(.leading, 35)
-                
-                VStack(alignment: .leading) {
-                    Text(book.name ?? "")
-                        .font(.bookTitle)
-                        .foregroundColor(.typo100)
-                    
-                    Text(book.author ?? "")
-                        .font(.bookCaption)
-                        .foregroundStyle(.typo80)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.bottom, 30)
-                    
-                    ForEach(["장르", "독서 상태", "독서 날짜"], id: \.self) { text in
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.backDarker)
+            .frame(width: 380, height: 180)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.typo25, lineWidth: 1)
+                    .overlay {
                         HStack {
-                            Text(text)
-                                .font(.bookCaption)
-                                .foregroundStyle(.typo50)
+                            fetchHomeImage(url: book.thumbnail ?? "")
                             
-                            if text == "독서 날짜" {
-                                Text("\(selectedDate, formatter: dateFormatter)")
-                                    .font(.bookCaptionBold)
-                                    .foregroundStyle(.typo100)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text(book.name ?? "")
+                                    .font(.bookTitle)
+                                    .foregroundColor(.typo100)
+                                    .kerning(-1)
+                                    .padding(.bottom, 3)
                                 
-                            } else if text == "장르" {
-                                Text(selectedGenre?.description ?? "미설정")
-                                    .font(selectedGenre != nil ? .bookCaptionBold : .bookCaption)
-                                    .foregroundStyle(.typo100)
+                                Text(book.author ?? "")
+                                    .font(.bookCaption)
+                                    .foregroundStyle(.typo80)
                                     .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(.leading, 26)
+                                    .padding(.bottom, 20)
                                 
-                            } else if text == "독서 상태" {
-                                Text(selectedReadStatus?.description ?? "미설정")
-                                    .font(selectedReadStatus != nil ? .bookCaptionBold : .bookCaption)
-                                    .foregroundStyle(.typo100)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                ForEach(["장르", "독서 상태", "독서 날짜"], id: \.self) { text in
+                                    HStack(spacing: 0) {
+                                        Text(text)
+                                            .font(.bookCaption)
+                                            .foregroundStyle(.typo50)
+                                            .padding(.trailing, 5)
+                                        
+                                        if text == "독서 날짜" {
+                                            Text("\(selectedDate, formatter: dateFormatter)")
+                                                .font(.bookCaptionBold)
+                                                .foregroundStyle(.typo100)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                            
+                                        } else if text == "장르" {
+                                            Text(selectedGenre?.description ?? "미설정")
+                                                .font(selectedGenre != nil ? .bookCaptionBold : .bookCaption)
+                                                .foregroundStyle(.typo100)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                                .padding(.leading, 26)
+                                            
+                                        } else if text == "독서 상태" {
+                                            Text(selectedReadStatus?.description ?? "미설정")
+                                                .font(selectedReadStatus != nil ? .bookCaptionBold : .bookCaption)
+                                                .foregroundStyle(.typo100)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                        }
+                                    }
+                                    .padding(.bottom, 3)
+                                }
                             }
+                            .padding(.leading, 15)
                         }
+                        .padding(.vertical, 30)
+                        .padding(.horizontal, 20)
                     }
-                }
-                .padding(.leading, 15)
-            }
+            )
             .padding(.top, 20)
-        }
     }
 }
 
