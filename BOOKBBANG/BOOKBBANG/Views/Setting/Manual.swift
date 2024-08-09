fee82064ad74045c4bb2689f42a6d9a9d5b77f7f
Guryss <lala1470@konkuk.ac.kr>
2024-08-09 11:36:54 +0900

    [Design] #64 - Manual View 이미지 추가 및 레이아웃 수정

--- BOOKBBANG/BOOKBBANG/Views/Setting/Manual.swift
+++ BOOKBBANG/BOOKBBANG/Views/Setting/Manual.swift
@@ -16,11 +16,11 @@ struct ManualChapter: Hashable {
 struct Manual: View {
     @Environment(\.dismiss) var dismiss
     let manualChapter: [ManualChapter] = [
-        ManualChapter(title: "책 반죽하기", image: "fish_1", context: "책은 우리가 만들 빵의 재료에요. 어떤 책으로 반죽을 만들어볼까요? 그 책은 어떤 맛인지 알려주세요!"),
-        ManualChapter(title: "책빵 굽기", image: "fish_2", context: "책 속 좋아하는 문장으로 빵을 구워보세요! 빵 반죽에 문장을 담아 맛있는 빵을 만들어 줄게요."),
-        ManualChapter(title: "오늘의 빵", image: "fish_3", context: "하루에 두 번, 내가 만든 빵들 중 하나를 추천해드려요. 설레는 마음으로 빵을 기다려보세요!"),
-        ManualChapter(title: "빵 클립", image: "fish_4", context: "이 빵을 언제 먹을지, 어떤 맛인지 빵 클립으로 묶어 보아요. 내가 먹고 싶을 때 더 쉽게 찾아 먹을 수 있어요!"),
-        ManualChapter(title: "빵수증", image: "fish_6", context: "일 년에 두 번, 내가 구운 빵들의 영수증이 만들어져요! 지금까지 구운 빵 기록들을 한 눈에 볼 수 있죠. 어떤 빵이 가장 많이 만들어졌는지 궁금하지 않나요? 빵을 많이 구울 수록 더 재미있는 빵수증이 될 거에요.")
+        ManualChapter(title: "책 반죽하기", image: "Baking_1", context: "책은 우리가 만들 빵의 재료에요.\n어떤 책으로 반죽을 만들어볼까요?\n그 책은 어떤 맛인지 알려주세요!"),
+        ManualChapter(title: "책빵 굽기", image: "Baking_3", context: "좋아하는 문장으로 빵을 구워보아요!\n책을 읽는 도중 인상 깊은 문장이 있다면\n저희에게 알려주세요.\n빵 반죽에 좋은 문장을 담아!\n맛있는 빵으로 만들어 줄게요."),
+        ManualChapter(title: "오늘의 빵", image: "Congratulatory_bread", context: "하루에 두 번, 내가 만든 맛있는 빵 중\n하나씩 추천해 드립니다.\n이번엔 어떤 빵이 나올지 기대되지 않나요?\n설레는 마음으로 빵을 열어보세요!"),
+        ManualChapter(title: "빵 클립", image: "CloverClip", context: "이 빵은 언제 먹으면 좋을지,\n어떤 특징을 가지고 있는지 클립으로 나누어보아요.\n내가 먹고싶은 빵이 있을 때 더 쉽게 찾아볼 수 있어요!"),
+        ManualChapter(title: "빵수증", image: "Receipt", context: "일 년에 두 번, 내가 구운 빵들의 대회가 열립니다.\n지금까지 빵을 구운 기록들을 한 눈에 볼 수 있죠.\n마치 영수증처럼요!\n어떤 빵이 가장 많이 만들어졌을지 궁금하지 않으세요?\n많은 빵을 구울 수록 더 재미있는 빵수증이 될 거예요.")
     ]
     var body: some View {
         VStack {
@@ -49,6 +49,7 @@ struct ShowEachChapter: View {
     var chapter: ManualChapter
     
     var body: some View {
+        
         VStack(spacing: 0) {
             Text(chapter.title)
                 .font(.teamMemberTitle)
@@ -62,6 +63,7 @@ struct ShowEachChapter: View {
             
             Text(chapter.context)
                 .font(.segment)
+                .kerning(-0.4)
                 .foregroundStyle(.typo100)
                 .frame(width: 330)
                 .multilineTextAlignment(.center)
@@ -85,7 +87,7 @@ struct ShowEachChapter: View {
 struct ManualCover: View {
     var body: some View {
         VStack {
-            Image("gentlemangyodong")
+            Image(.gentlemangyodong)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 180, height: 180)
