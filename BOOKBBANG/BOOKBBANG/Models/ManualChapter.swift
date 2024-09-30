a4e9930524d65ba64b973f55fd651ee9831a5850
Guryss <lala1470@konkuk.ac.kr>
2024-10-01 00:23:28 +0900

    [Chore] #84 - ManualChapter 내 수정된 라이팅 반영

--- BOOKBBANG/BOOKBBANG/Models/ManualChapter.swift
+++ BOOKBBANG/BOOKBBANG/Models/ManualChapter.swift
@@ -5,29 +5,29 @@
 //  Created by Seoyeon Choi on 8/12/24.
 //
 
-import Foundation
+import SwiftUI
 
-struct ManualChapter: Hashable {
+struct ManualChapter {
     var title: String
-    var image: String
+    var image: Image
     var content: String
 }
 
 let manualChapter: [ManualChapter] = [
-    ManualChapter(title: "책 반죽하기", 
-                  image: "Baking_1",
-                  content: "책은 우리가 만들 빵의 재료에요.\n어떤 책으로 반죽을 만들어볼까요?\n그 책은 어떤 맛인지 알려주세요!"),
+    ManualChapter(title: "책 반죽하기",
+                  image: Image(.mustacheCat),
+                  content: "빵을 만들려면 재료가 필요하죠\n어떤 책을 재료로 반죽을 만들어볼까요?\n그리고 이 책은 어떤 맛이 나는지 들려주세요"),
     ManualChapter(title: "책빵 굽기",
-                  image: "Baking_3", 
-                  content: "책 속 좋아하는 문장으로 빵을 구워보아요!\n빵 반죽에 문장을 담아!\n맛있는 빵으로 만들어 줄게요."),
-    ManualChapter(title: "오늘의 빵",
-                  image: "Congratulatory_bread",
-                  content: "하루에 두 번, 내가 만든 빵들 중\n하나씩 추천해 드립니다.\n설레는 마음으로 빵을 열어보세요!"),
+                  image: Image(.bookCat),
+                  content: "좋아하는 문장으로 빵을 구워요\n책을 읽는 도중 인상 깊은 문장이 있다면 저희에게 알려주세요\n빵 반죽에 좋은 문장을 담아 맛있는 빵으로 만들어 줄께요"),
+    ManualChapter(title: "오늘의 추천 빵",
+                  image: Image(.eatCat),
+                  content: "하루에 두 번, 내가 만든 맛있는 빵 중\n하나를 추천해 드립니다\n설레는 마음으로 빵을 열어보세요\n\n[ 빵 나오는 시간 : 매일 오전 9시, 오후 6시 ]"),
     ManualChapter(title: "빵 클립",
-                  image: "CloverClip",
-                  content: "이 빵을 언제 먹을지, 어떤 맛인지\n 빵 클립으로 나누어보아요.\n내가 먹고싶을 때 더 쉽게 찾아 먹을 수 있어요!"),
+                  image: Image(.pencilCat),
+                  content: "이 빵은 언제 먹으면 좋을지,\n어떤 특징을 가지고 있는지 클립으로 나누어 보아요\n내가 먹고 싶은 빵이 있을 때 더 쉽게 찾아볼 수 있어요"),
     ManualChapter(title: "빵수증",
-                  image: "Receipt",
-                  content: "일 년에 두 번, 내가 구운 빵들의 영수증이 만들어져요!.\n지금까지 빵을 구운 기록들을 한 눈에 볼 수 있죠.\n어떤 빵이 가장 많이 만들어졌을지 궁금하지 않나요?\n많은 빵을 구울 수록 더 재미있는 빵수증이 될 거예요.")
+                  image: Image(.completeCat),
+                  content: "일년에 두 번, 내가 구운 빵들의 대회가 열립니다\n지금까지 빵을 구운 기록들을 한 눈에 볼 수 있습니다\n많은 빵을 구울수록 더 재미있는 빵수증이 될 거에요")
 ]
 
