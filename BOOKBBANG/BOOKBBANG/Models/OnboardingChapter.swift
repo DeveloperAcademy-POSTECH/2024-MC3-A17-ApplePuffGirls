d59aa3dce13d4fc13142ff44402f49d89dcfdbf3
Guryss <lala1470@konkuk.ac.kr>
2024-10-10 15:34:04 +0900

    [Design] #84 - 메뉴얼 이미지 수정 및 온보딩 레이아웃 수정

--- BOOKBBANG/BOOKBBANG/Models/OnboardingChapter.swift
+++ BOOKBBANG/BOOKBBANG/Models/OnboardingChapter.swift
@@ -16,7 +16,7 @@ struct OnboardingChapter: Hashable {
 
 let onboardingChapter: [OnboardingChapter] = [
     OnboardingChapter(firstImage: "onboarding1",
-                      firstContent: "책빵이라는 빵집이 있었어요",
+                      firstContent: "'책빵'이라는 빵집이 있었어요",
                       secondImage: "onboarding2",
                       secondContent: "책빵장인 냥스터는 빵을 만들 수 없어서 힘든 나날들을 보내고 있었어요"),
     OnboardingChapter(firstImage: "onboarding5",
@@ -28,7 +28,7 @@ let onboardingChapter: [OnboardingChapter] = [
                       secondImage: "onboarding4",
                       secondContent: "왜냐면 사람들이 책을 하나도 읽지 않거든요"),
     OnboardingChapter(firstImage: "onboarding7",
-                      firstContent: "그래서 파리만 날리는 책빵 베이커리 ..",
+                      firstContent: "그래서 파리만 날리는 '책빵 베이커리' ..",
                       secondImage: "onboarding8",
                       secondContent: "책빵장 냥스터를 도와주세요!")
 ]
